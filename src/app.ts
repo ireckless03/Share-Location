a92ba95335780d1f0ab120169648b0d2412e2bcf
Denny Doan <denny-333@hotmail.com>
2023-07-17 12:31:39 -0400

    Added event listener to retrieve search address

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,16 @@
-// Code goes here!
+// gives us access to form
+const form = document.querySelector("form")!;
+const addressInput = document.getElementById('address')! as HTMLInputElement;
+
+// retrieving search address
+function searchAddressHandler(event: Event) {
+  event.preventDefault();
+  
+  const enteredAddress = addressInput.value;
+  console.log('Entered address:', enteredAddress);
+
+  // send search to googles api
+}
+
+// adding event listener to form
+form.addEventListener('submit', searchAddressHandler);
